4c492ac3ddf23f084cc66f84c24d8563f1a206a5
unknown <you@example.com>
2021-11-05 10:33:50 +0530

    1st file

---
+++ test.js
@@ -0,0 +1,37 @@
+console.log("welcome to js one program");
+//variables
+var a = 2;
+console.log(a);
+const finalvalue =5;
+
+// finalvalue =6;
+function test(){
+    console.log("from function",a)
+    // let d =2;
+}
+var c = 2;
+// console.log("value of d is",d);
+if(c ==2){
+    var w = 2;
+    //  console.log("w is",w) 
+}
+console.log("w is",w)   
+test();
+var a =2+3;
+function add(){
+    console.log( "add the value ",a)
+}
+add();
+const d =123;
+console.log(d)
+console.log("final value is",finalvalue);
+var x=7;
+console.log(x);
+// variable is redeclared
+var x=8;
+console.log(x);
+// let variable is not redeclared
+let y =9;
+console.log(y);
+// let y =10;
+// console.log(y);
