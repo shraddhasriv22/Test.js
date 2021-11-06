7957a6e4764cd69a154c808384dc386ef4c604f5
unknown <you@example.com>
2021-11-06 14:37:12 +0530

    add new file

---
+++ Arithmetic.js
@@ -0,0 +1,67 @@
+var add = 2+2;
+console.log("add is equal to",add);
+var subtract = 4-2;
+console.log("subtract is equal to",subtract);
+var multiply = 4*2;
+console.log("multiply is equal to",multiply);
+var divide = 4/2;
+console.log("divide is equal to",divide);
+var remainder = 5%2;
+console.log("remainder is equalto",remainder);
+// var power = 2**2;
+// console.log("power",power);
+// =,==,===
+// var a =2;
+// var b= a;
+// console.log("a is equals to",b);
+var a=2;
+var b=2;
+console.log(" a is equal to ",a==b);
+var a=2;
+var b="2";
+console.log(" a is equal to ",a==b);
+var a=2;
+var b="2";
+console.log(" a is equal to ",a===b,typeof(b));
+var a=2;
+var b=2;
+console.log(" a is equal to ",a===b);
+var a=5;
+var c=7
+// console.log("type of c is ",typeof(c),a>c);
+console.log(a<c);
+var a=5;
+var c=7
+console.log(a!==c);
+//increment,decrement
+var a=5;
+// a++;
+// a--;
+// console.log(--a);
+console.log(a++);
+var c= 4; 
+var d=6;
+// && (when all are true)
+if(c==4&& d==6)
+console.log("in ifstatement");
+if(c==5||d==6)
+console.log("in ifstatement");
+if(c!==7)
+console.log("in ifstatement");
+if(a==10){
+    console.log("a is equal ")
+}
+else if(d==6){
+    console.log("a is not equal to ")
+}
+else{
+    console.log("no match")
+}
+var a=9;
+var b=8;
+console.log("ternary operator", a==6? a():"2part")
+var commaop=(2,3,5)
+console.log(commaop)
+var comma1=(2,3)
+var comma2=(3,4)
+console.log("comma",comma2)
